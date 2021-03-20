affb48e609eb92259df650a26f77dac094245156
Blair Conrad <blair@blairconrad.com>
2021-03-19 22:04:00 -0400

    SAVE

--- assets/build_icons.py
+++ assets/build_icons.py
@@ -9,10 +9,20 @@ except:  # NOQA - fallback, no matter the cause
 
 original = Image.open("makeiteasy_original.png")
 cropped = original.crop((63, 11, 631, 579))
-for width in (512, 256, 128, 64, 48, 32, 16):
-    resized = cropped.resize((width, width), Image.BICUBIC)
-    resized.save(f"makeiteasy_{width}.png")
-social = Image.new("RGB", (cropped.size[1] * 2, cropped.size[1]), "white")
-social.paste(cropped, (0, 0))
-social = social.resize((640, 320), Image.BICUBIC)
-social.save("social-preview.png")
+for background in ("white", "black", ""):
+    for width in (512, 256, 128, 64, 48, 32, 16):
+        resized = cropped.resize((width, width), Image.BICUBIC)
+
+        filename = f"makeiteasy_{background}_{width}.png".replace("__", "_")
+        icon = Image.new("RGBA", (width, width), background or "#00000000")
+        icon.paste(resized, mask=resized)
+        icon.save(filename)
+
+        filename = f"makeiteasy_{background}_social_preview.png".replace("__", "_")
+
+    social = Image.new(
+        "RGBA", (cropped.size[1] * 2, cropped.size[1]), background or "#00000000"
+    )
+    social.paste(cropped, mask=cropped)
+    social = social.resize((640, 320), Image.BICUBIC)
+    social.save(filename)
